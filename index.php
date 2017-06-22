934a2288daf600a35d6124f75d4fea2c587e8191
andydiwara <andy_di_wara@hotmail.com>
2017-06-22 17:04:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,33 +4,6 @@ $access_token = '/SCsycS09R2V1mWSUnNKmupMca498c8saeYCO/1uTrep9Hw7TCCpM9CoJI0ahEw
 $content = file_get_contents('php://input');
 ///
 
-{
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
-}
 
 ///
 // Parse JSON
