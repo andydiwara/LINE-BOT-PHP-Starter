e6a7be9d18f29aa4d8604e583d66e1ef7bf51add
andydiwara <andy_di_wara@hotmail.com>
2017-06-22 17:02:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,37 @@
 $access_token = '/SCsycS09R2V1mWSUnNKmupMca498c8saeYCO/1uTrep9Hw7TCCpM9CoJI0ahEw7P4HlfPMNKdKiN0WoXooPLi6ffTjlXhMddxPheADN2OgYdqVdr6UntJNBHnGP4TEVn+YhFEvd30AA8f4vQDeKigdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
+///
+
+{
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}
+
+///
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
@@ -44,31 +75,3 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-
-{
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
-}
